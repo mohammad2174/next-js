61e2984c6564309ec3019773e33f2065e305b6d2
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2022-05-15 10:34:35 +0430

    Dynamic Rendering

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -6,8 +6,8 @@ const ArticleList = ({ article }) => {
     return (
         <Link href="/article/[id]" as={`/article/${article.id}`}>
             <a>
-                <h3>{article.title.toUppercase()} &rarr;</h3>
-                <p>{article.body}</p>
+                <h3>{article.title} &rarr;</h3>
+                {/* <p>{article.body}</p> */}
             </a>
         </Link>
     )
